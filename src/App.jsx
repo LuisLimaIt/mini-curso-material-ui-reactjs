40743c46374a4afbac37faaa916e33f0d602148c
LuisLimaIt <pls.luis@gmail.com>
2021-11-24 18:22:08 -0300

    AppBar o Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
+import { ThemeProvider } from "@material-ui/core";
 import React from "react";
-import Buttons from './material/Buttons'
-import CustomButtons from "./material/CustomButtons";
-import GlobalTheme from "./material/GlobalTheme";
-import Icons from "./material/Icons";
-import TypographyExample from "./material/TypographyExample";
+import theme from "./themeConfig"
+import Button from '@material-ui/core/Button'
+import Navbar from "./components/Navbar";
+import Typography from '@material-ui/core/Typography'
 
 function App() {
   return (
-    <>
-    <div>
-      <Buttons />
-    </div>
-    <div style={{borderTop: "2px solid #000", marginTop: "20px"}}>
-      <Icons />
-    </div>
-    <div style={{borderTop: "2px solid #000", marginTop: "20px"}}>
-      <TypographyExample />
-    </div>
-    <div style={{borderTop: "2px solid #000", marginTop: "20px"}}>
-      <CustomButtons />
-    </div>
-    <div style={{borderTop: "2px solid #000", marginTop: "20px"}}>
-      <GlobalTheme />
-    </div>
-    </>
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <Button variant="contained" color="primary">
+        Botão
+      </Button>
+      <Button variant="contained" color="secondary">
+        Botão
+      </Button>
+      <Typography variant="h1" color="initial">
+      Mussum Ipsum, cacilds vidis litro abertis. Paisis, filhis, espiritis santis. Per aumento de cachacis, eu reclamis. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.
+      </Typography>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { AppBar, IconButton, makeStyles, Toolbar, Typography } from '@material-ui/core'
+
+import MenuIcon from '@material-ui/icons/Menu'
+import Button from '@material-ui/core/Button'
+const useStyles = makeStyles(theme => ({
+    offset: theme.mixins.toolbar,
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1
+    }
+}));
+
+const Navbar = () => {
+    const classes = useStyles();
+    return (
+        <div>
+            <AppBar>
+              <Toolbar>
+                    <IconButton 
+                        color="inherit" 
+                        aria-label='menu'
+                        classname={classes.menuButton} 
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                    Luis
+                </Typography>
+                <Button variant="text" color="inherit">
+                  Login
+                </Button>
+              </Toolbar>
+            </AppBar>
+            <div className={classes.offset}></div>
+        </div>
+    )
+}
+
+export default Navbar;
