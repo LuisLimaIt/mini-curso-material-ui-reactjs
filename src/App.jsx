85fa627b53104d94464ecff060c569543cc71359
LuisLimaIt <pls.luis@gmail.com>
2021-11-24 12:51:37 -0300

    MakeStyle CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Buttons from './material/Buttons'
+import CustomButtons from "./material/CustomButtons";
 import Icons from "./material/Icons";
 import TypographyExample from "./material/TypographyExample";
 
@@ -14,6 +15,9 @@ function App() {
     </div>
     <div style={{borderTop: "2px solid #000", marginTop: "20px"}}>
       <TypographyExample />
+    </div>
+    <div style={{borderTop: "2px solid #000", marginTop: "20px"}}>
+      <CustomButtons />
     </div>
     </>
   );
--- src/material/TypographyExample.jsx
+++ src/material/TypographyExample.jsx
@@ -5,16 +5,16 @@ import Typography from '@material-ui/core/Typography'
 const TypographyExample = (props) => (
     <>
         <Typography variant="h1" color="primary">
-            Parâmetro variante é um H1
+            Parâmetro variant é = h1
         </Typography>
         <Typography variant="body1" color="secondary" align="center" paragraph>
-            Lorem ipsum issum es calypsum
+        Parâmetro variant é = body1
         </Typography>
         <Typography variant="body1" color="primary" align="center" gutterBottom>
             Lorem ipsum
         </Typography>
         <Typography variant="body1" color="initial" align="center">
-            Lorem ipsum netflics elevenes dances
+            Lorem ipsum netflics elevenes telecineses
         </Typography>
     </>
 );
